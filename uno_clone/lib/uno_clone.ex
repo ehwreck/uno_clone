8d2924226a67232aa3a639bdd9d897bdee2283b0
Erick Trevino <erick.trevino@cloudlite.io>
2022-10-05 20:07:59 -0500

    Uno_clone project created.

---
+++ uno_clone/lib/uno_clone.ex
@@ -0,0 +1,18 @@
+defmodule UnoClone do
+  @moduledoc """
+  Documentation for `UnoClone`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UnoClone.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ uno_clone/test/uno_clone_test.exs
@@ -0,0 +1,8 @@
+defmodule UnoCloneTest do
+  use ExUnit.Case
+  doctest UnoClone
+
+  test "greets the world" do
+    assert UnoClone.hello() == :world
+  end
+end
